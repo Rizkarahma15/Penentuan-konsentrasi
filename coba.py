624660c9c2aea8755589b6af542d016638c73324
Rizkarahma15 <rizkarahma1972@gmail.com>
2025-05-10 10:08:05 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image  # Pastikan juga ada PIL, meski tidak dipakai di kode ini, pastikan librarynya terinstall
+from PIL import Image  # Pastikan library ini terinstal jika digunakan
 
 # Buat requirements.txt otomatis
 if not os.path.exists('requirements.txt'):
