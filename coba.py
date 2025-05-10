fb4dff0703195eeeac4e933c0b4167e1b855c880
Rizkarahma15 <rizkarahma1972@gmail.com>
2025-05-10 10:39:01 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -5,11 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
 
-# Buat requirements.txt otomatis
-if not os.path.exists('requirements.txt'):
-    with open('requirements.txt', 'w') as f:
-        f.write("streamlit\nnumpy\npandas\nmatplotlib\nPillow\n")
-
 # Fungsi regresi
 def calculate_regression_equation(X, Y, var_name_x='x', var_name_y='y'):
     n = len(X)
@@ -22,7 +17,7 @@ def calculate_regression_equation(X, Y, var_name_x='x', var_name_y='y'):
     b = (n * sum_xy - sum_x * sum_y) / (n * sum_x_squared - sum_x**2)
     a = (sum_y - b * sum_x) / n
 
-    r = (n * sum_xy - sum_x * sum_y) / np.sqrt((n * sum_x_squared - sum_x*2) * (n * sum_y_squared - sum_y*2))
+    r = (n * sum_xy - sum_x * sum_y) / np.sqrt((n * sum_x_squared - sum_x**2) * (n * sum_y_squared - sum_y**2))
 
     equation = f'{var_name_y} = {a:.2f} + {b:.2f}{var_name_x}'
     return {'equation': equation, 'intercept': a, 'slope': b, 'r_value': r}
@@ -129,4 +124,3 @@ def main():
 # Pastikan ini ditulis dengan benar
 if __name__ == '__main__':
     main()
-
